5fd3730662c9f71397965c11b56413dd6b9a5ca4
Griss819 <rafaeliglesias819@gmail.com>
2025-07-16 15:15:42 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import ProductList from "@/app/ui/product/product-list";
+import ProductFilter from "@/app/ui/product/product-filter";
+import {createCart, fetchCategories} from "@/app/lib/data";
+import {Suspense} from "react";
+
+export default async function Home(props: {
+  searchParams?: Promise<{
+    name?: string;
+    category?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  await createCart();
+  return (
+    <div className={'product-display'}>
+      <Suspense fallback={(<div><em className={'fa fa-circle-o-notch fa-spin'}></em> Loading</div>)}>
+        <div>
+          <ProductFilter />
+        </div>
+        <ProductList name={searchParams?.name} category={searchParams?.category} />
+      </Suspense>
+    </div>
+  );
+}
